c804438a1d2ff461f699eabaac8001bd46c3d0ec
AgustinBartolini <92292613+AgustinBartolini@users.noreply.github.com>
2021-11-15 18:44:14 +0100

    Add files via upload

---
+++ moneda.js
@@ -0,0 +1,9 @@
+let moneda = Math.floor(Math.random() * 2);
+
+    if (moneda == 0) document.write ("Cruz");
+
+    if (moneda == 1) document.write ("Cara");
+
+    function refresh(){
+        window.location.reload("Refresh")
+      }
